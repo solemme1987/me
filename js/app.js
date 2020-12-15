d0b4c8418056cf7da960e0e27311eb359672c5af
Ricardo Cortes <ricacor3@gmail.com>
2020-12-15 18:21:40 -0500

    creo el menu desplegable

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function activarMenu() {
+
+    let menuButtom = document.querySelector(".toggle");
+    let menu = document.querySelector(".menu");
+    menuButtom.classList.toggle("active");
+    menu.classList.toggle("active");
+}
