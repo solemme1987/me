a7d27e7db303c00f0a3c45f25d8a5dd6f2ddaad4
Ricardo Cortes <ricacor3@gmail.com>
2020-12-16 11:11:25 -0500

    algunos responsive que no me gustan

--- js/app.js
+++ js/app.js
@@ -4,4 +4,6 @@ function activarMenu() {
     let menu = document.querySelector(".menu");
     menuButtom.classList.toggle("active");
     menu.classList.toggle("active");
+    console.log("Inner width", alturaDelViewport = window.innerWidth);
+    console.log("Inner heigt", alturaDelViewport = window.innerHeight);
 }
